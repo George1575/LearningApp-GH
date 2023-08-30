c05960c6f40413fe3aed8c028c9612c912904bc8
George <georgehargreaves5@outlook.com>
2023-08-30 10:11:03 +0100

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by George Hargreaves on 30/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
